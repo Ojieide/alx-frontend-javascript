06c296daf662871af586e8077b414d2bf3fc431d
Ojieide Egbomondion <ojieide@yahoo.com>
2023-09-25 23:52:02 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,13 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars
     const task = true;
-    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
+  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }
-
